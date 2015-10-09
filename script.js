c14ef64103e14d94283ba61627989d599f541000
Mackplace <ma.cifuentes51@uniandes.edu.co>
2015-10-08 21:20:25 -0500

    fds
    dsfs

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-$(document).read(function(){
-        $.getJSON("", function(json) {
-            console.log(json); // this will show the info it in firebug console
+
+var songs= [];
+
+
+$(document).ready(function(json){
+    $.getJSON("https://raw.githubusercontent.com/Mackplace/jsjson/gh-pages/playlist.json", function(json) {
+
+        $.each( json, function(i, llave ) {
+            $.each( llave, function( j, lala ) {
+                songs.push(lala);
+            });
         });
+        actualizacion();
+        // this will show the info it in firebug console
+    });
+});
+
+function actualizacion()
+{
+
+    // Obtiene la plantilla
+    var fuentePlantilla = $("#tablaCanciones").html();
+
+    // Compila la plantilla
+    var plantilla = Handlebars.compile( fuentePlantilla );
+
+    // Pasa los datos a la plantilla
+    var html = plantilla( songs );
+
+    // Agrega el resultado a la página
+    $('#tablaSongs').html( html );
+
 }
-);
+
+
